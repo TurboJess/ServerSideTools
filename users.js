44bd79783883af4d02337fbc6d94c553b7905f93
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-19 15:15:54 -0500

    created dynamic table that pulls the Json data through a ajax request using jQuery

--- users.js
+++ users.js
@@ -1,42 +1,28 @@
-$(document).ready(function() {
-   $.ajax({
-       'url': "http://54.213.92.70:8080/getaccount",
-		'type': 'GET',
-		dataType: "json",
-		'data':{
-			'account' : 'all'
-		},
-		}).then(function(data) {
-		$('.request').append(data.request);
-		$('.ack').append(data.ack);
-		$('.return_info').append(data.return_info);
-		$('.turbo_user_id').append(data.turbo_user_id);
-		$('.name').append(data.name);
-		$('.session_token').append(data.session_token);
-		   });
+$(document).ready(function () {
+    $.ajax({
+        url: "http://54.213.92.70:8080/getaccount",
+        type: 'GET',
+        dataType: "json",
+        data: {
+            'account': 'all'
+        },
+    }).then(function (data) {
+        for(var i = 0; i < data.accounts.length; i++){
+            $('#myTable tbody').append("<tr>");
+            // $('#myTable').append("<td class=\"photo_url\">" + data.accounts[i].photo_url + "</td>");
+            $('#myTable tbody').append("<td class=\"user_id\">" + data.accounts[i].turbo_user_id + "</td>");     
+            $('#myTable tbody').append("<td class=\"name\">" + data.accounts[i].name + "</td>");
+            $('#myTable tbody').append("<td class=\"session_token\">" + data.accounts[i].session_token + "</td>");
+            $('#myTable tbody').append("<td class=\"photo_url\">" + data.accounts[i].photo_url + "</td>");   
+            $('#myTable tbody').append("</tr><br>"); 
+
+        }
+    });
+
+    console.log('finished');
 });
 
 
 
 
 
-
-// $(document).ready(function(){
-
-// 	$.ajax({
-// 		url:"http://54.213.92.70:8080/getaccount",
-//         dataType: 'json',
-//         type:"GET",
-//         success: function(response) {
-//             jsonData = response;
-//         }
-// 		data:{
-// 			account: ""
-// 		},
-		
-
-		
-// 	})
-// 		console.log(account);
-
-// });
