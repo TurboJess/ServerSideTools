4b120aa9896e68578967000682bd5a6a1dd4abdf
Jessica Waite <ImJessW@Jessica-Waites-MacBook-Pro.local>
2014-02-20 10:23:07 -0500

    I was able to get  the image to display from the json data. I broke down the code with comments on how the code was working. I also gave the table a bit of padding to the left and right to reduce crowding.

--- users.js
+++ users.js
@@ -6,18 +6,26 @@ $(document).ready(function () {
         data: {
             'account': 'all'
         },
+        // once json data is retrieved with the get request using ajaax then display teh following table rows.
     }).then(function (data) {
+        // for loop for looping through th elenght o fth json data
         for(var i = 0; i < data.accounts.length; i++){
+            // creates the table row every time a new instance is created
             $('#myTable tbody').append("<tr>");
-            // $('#myTable').append("<td class=\"photo_url\">" + data.accounts[i].photo_url + "</td>");
-            $('#myTable tbody').append("<td class=\"user_id\">" + data.accounts[i].turbo_user_id + "</td>");     
+            // pulls image source for each individule user and displays it 
+            $('#myTable tbody').append("<td class=\"photo_url\">" + ("<img src='" + data.accounts[i].photo_url + "'/>") + "</td>");
+            // displays Turbo user name in a new instance of a user
+            $('#myTable tbody').append("<td class=\"user_id\">" + data.accounts[i].turbo_user_id + "</td>"); 
+            // displays user real name in  
             $('#myTable tbody').append("<td class=\"name\">" + data.accounts[i].name + "</td>");
+            // displays individule session token for each user.
             $('#myTable tbody').append("<td class=\"session_token\">" + data.accounts[i].session_token + "</td>");
-            $('#myTable tbody').append("<td class=\"photo_url\">" + data.accounts[i].photo_url + "</td>");   
+            // $('#myTable tbody').append("<td class=\"photo_url\">" + data.accounts[i].photo_url + "</td>");   
             $('#myTable tbody').append("</tr><br>"); 
 
         }
     });
+       
 
     console.log('finished');
 });
